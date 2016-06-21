4c03be1f3d3fc0ecb041016333d7601ca5da2226
Dale Seo <dale.seo@gmail.com>
2016-06-22 00:18:47 +0900

    POST /todos

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const svc = require('./svc');
+
 var app = express();
+app.use(bodyParser.json());
 app.listen(3000, () => {
   console.log("This server is listening through port 3000.")
 });
 
-app.get('/todos/', (req, res) => {
+app.get('/todos', (req, res) => {
   res.send(svc.list());
 });
 
 app.get('/todos/:id', (req, res) => {
   res.send(svc.detail(req.params.id));
 });
+
+app.post('/todos', (req, res) => {
+  console.log(req.body);
+  res.status(201).send(svc.create(req.body));
+})
--- svc.js
+++ svc.js
@@ -4,9 +4,16 @@ module.exports = {
   },
   detail: function(id) {
     return list[id];
+  },
+  create: function(obj) {
+    obj.id = counter++;
+    console.log(obj);
+    list.push(obj);
+    return {id: obj.id};
   }
 };
 
+var counter = 4;
 var list = [
   {id : 1, title: 'HTML', description: 'Hyper Text Markup Language'},
   {id : 2, title: 'CCS', description: 'Cascading Style Sheet'},
